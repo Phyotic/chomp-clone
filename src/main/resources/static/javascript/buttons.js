2da36d3e3da2ca3e4acefa209b7faaf4484b448b
Daniel Solorzano <dsolo.prof@gmail.com>
2023-06-02 01:34:33 -0500

    Created menu info retrieval from file + populate home page menu.

--- src/main/resources/static/javascript/buttons.js
+++ src/main/resources/static/javascript/buttons.js
@@ -7,7 +7,23 @@ cartButton.addEventListener("click", toggleAside);
 blackout.addEventListener("click", toggleAside);
 checkoutClose.addEventListener("click", toggleAside);
 
-// FUNCTIONS
+// Load proper cart status element
+if(localStorage.getItem("cc-cart-items")) {
+    loadCartContains();
+    //TODO: call fillCart();
+} else {
+    loadCartEmpty();
+}
+
+// Fetch the menu and load it into local storage.
+fetchMenuToStorage()
+.then(() => populateMenu("menu-grid", "burgers")
+    .catch(error => console.error("Could not populate menu.", error)))
+.catch(error => console.error("Could not load/retrieve menu: ", error));
+
+// == FUNCTIONS ==
+
+//Toggle cart overlay.
 function toggleAside(event) {
     const aside = document.getElementById("checkout-aside");
     
@@ -15,5 +31,107 @@ function toggleAside(event) {
         aside.style.display = "flex";
     } else {
         aside.style.display = "none";
+    }
+}
+
+//Load non-empty cart element
+function loadCartContains() {
+    const checkoutBottomSection = document.getElementById("checkout-bottom-section");
+
+    checkoutBottomSection.style.alignItems = "initial";
+
+    fetch("../templates/NonEmptyCart.html")
+    .then(response => response.text())
+    .then(nonEmptyTemplate => {
+        if(checkoutBottomSection.childElementCount === 0) {
+            checkoutBottomSection.innerHTML = nonEmptyTemplate;
+        } else {
+            if(!checkoutBottomSection.firstElementChild.id.match("non-empty-cart")) {
+                checkoutBottomSection.replaceChild(checkoutBottomSection.firstElementChild, nonEmptyTemplate);
+            }
+        }
+    })
+}
+
+//TODO: Fill cart with stored items
+function fillCart() {
+
+}
+
+//Load empty cart element
+function loadCartEmpty() {
+    const checkoutBottomSection = document.getElementById("checkout-bottom-section");
+    fetch("../templates/EmptyCart.html")
+    .then(response => response.text())
+    .then((emptyTemplate) => {
+        if(checkoutBottomSection.childElementCount === 0) {
+            checkoutBottomSection.innerHTML = emptyTemplate;
+        } else {
+            if(!checkoutBottomSection.firstElementChild.id.match("checkout-empty")) {
+                checkoutBottomSection.replaceChild(checkoutBottomSection.firstElementChild, template);
+            }
+        }
+    })
+    .catch(error => {
+        console.error(error);
+    });
+}
+
+//Fetch the complete menu and save to local storage.
+async function fetchMenuToStorage() {
+    fetch("menu.json")
+    .then((response) => response.text())
+    .then((ccMenu) => {
+        localStorage.setItem("cc-menu", ccMenu);
+    })
+}
+
+//Load the specified sub menu from local storage.
+function loadMenuFromStorage(kind) {
+    if(localStorage.getItem("cc-menu")) {
+        const completeMenu = JSON.parse(localStorage.getItem("cc-menu"));
+    
+        for(const menuObj of completeMenu) {
+            if(menuObj.name === kind) {
+                return menuObj;
+            }
+        }
+
+        console.error("Provided key is not in stored data.")
+        return null;
+    } else {
+        console.error("No data found in local storage");
+        return null;
+    }
+}
+
+//Load the the specified menu under the specified parent element.
+async function populateMenu(parentName, kind) {
+    const menuObj = loadMenuFromStorage(kind);
+
+    if(menuObj) {
+        const itemsArray = menuObj.menuItems;
+
+        const parent = document.getElementById(parentName);
+        parent.replaceChildren();
+    
+        fetch("../templates/MenuItem.html")
+        .then((response) => response.text())
+        .then((template) => {
+            for(const item of itemsArray) {
+                let copyTemp = template.slice();
+                let newItem = copyTemp
+                    .replace("{{itemName}}", item.name)
+                    .replace("{{itemPrice}}", item.price.toFixed(2))
+                    .replace("{{imageSource}}", item.imageUrl);
+    
+                parent.innerHTML += newItem;
+            }
+        })
+        .catch(error => {
+            console.error("Could not retrieve template resource.", error);
+        });
+    } else {
+        console.error("Could not retrieve sub menu from storage.")
     }
 }
